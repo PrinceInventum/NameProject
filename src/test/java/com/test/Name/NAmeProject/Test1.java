fabe3df3f3e59a1c6ca87d948fd0c1f7ef3b1300
PrinceInventum <prince.ambast@inventum.net>
2020-03-02 12:52:43 +0530

    first Unify commit

---
+++ src/test/java/com/test/Name/NAmeProject/Test1.java
@@ -0,0 +1,15 @@
+package com.test.Name.NAmeProject;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Test 1");
+
+		
+		System.out.println("Test2");
+		
+	}
+
+}
